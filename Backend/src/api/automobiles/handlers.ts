bd76cf77f3d395de3890e8e064ea5259c2f00bac
AbdulkarimOgaji <2010704039@udusok.edu.ng>
2022-05-10 07:37:26 +0100

    pagination for getAllautomobiles

--- Backend/src/api/automobiles/handlers.ts
+++ Backend/src/api/automobiles/handlers.ts
@@ -2,7 +2,10 @@ import { Request, Response } from 'express';
 import { AutoMobile } from './models';
 
 const getAllAutoMobiles = (req: Request, res: Response) => {
-    AutoMobile.find()
+    const { pageId, pageSize } = req.params
+    const limit = parseInt(pageSize)
+    const skip = (parseInt(pageId) - 1) * parseInt(pageSize)
+    AutoMobile.find({}, {}, {limit, skip})
       .then((result) =>
         res.json({
           success: true,
@@ -18,5 +21,78 @@ const getAllAutoMobiles = (req: Request, res: Response) => {
         })
       );
   };
+
+const createAutoMobile = (req: Request, res: Response) => {
+    const automobile = new AutoMobile(req.body)
+    automobile
+    .save()
+    .then(result => {
+      res.status(201).json({
+        status: "success",
+        message: "created automobile successfully",
+        error: null,
+        data: result,
+      })
+    })
+    .catch((err => {
+      res.status(500).json({
+        status: "failure",
+        message: "Failed to create automobile",
+        error: err,
+        data: null,
+      })
+    }))
+}
+
+const getAutoMobileById = (req: Request, res:Response) => {
+  const id = req.params.id
+  AutoMobile.findById(id)
+  .then(result => {
+    res.json({
+      status: "success",
+      message: "fetched automobile successflly",
+      error: null,
+      data: result,
+    })
+  })
+  .catch(err => res.status(500).json({
+    status: "Failure",
+    message: "Failed to fetch Automobile",
+    error: err,
+    data: null,
+  }))
+}
+
+
+const updateAutoMobile = () => {}
+
+const deleteAutoMobile = (req: Request, res: Response) => {
+  const id = req.params.id
+  AutoMobile.deleteOne({ _id: id })
+  .then(result => {
+    res.json({
+      status: "Success",
+      message: "Automobile deleted successfully",
+      data: result,
+      error: null,
+    })
+  })
+  .catch(err => {
+    res.status(500).json({
+      status: "Failure",
+      message: "Failed to delete automobile",
+      error: err,
+      data: null,
+    })
+  })
+}
+const handlers = {
+  getAllAutoMobiles,
+  getAutoMobileById,
+  createAutoMobile,
+  deleteAutoMobile
+}
+
+export default handlers
 
 
