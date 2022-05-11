2ccf3c3a10f049c278272bcb0ab806a61517b26c
AbdulkarimOgaji <2010704039@udusok.edu.ng>
2022-05-11 15:45:24 +0100

    load frontend_url from process.env

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -12,7 +12,7 @@ const app: Express = express();
 // allow access origin
 app.use(function (req, res, next) {
 
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', secretsConfig.frontEndUrl);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
